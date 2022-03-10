9f2a3c055d70e78db26e7a465a24bfd8c8b3a051
Nima <nimahakim.ad@gmail.com>
2022-03-10 15:22:29 +0330

    add pemission for camera and storage

--- app/src/main/java/com/company/nima/favdish/view/activities/AddUpdateDishActivity.kt
+++ app/src/main/java/com/company/nima/favdish/view/activities/AddUpdateDishActivity.kt
@@ -1,22 +1,56 @@
 package com.company.nima.favdish.view.activities
 
+import android.Manifest
+import android.app.AlertDialog
 import android.app.Dialog
+import android.content.ActivityNotFoundException
+import android.content.DialogInterface
+import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.provider.Settings
 import android.view.View
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import com.company.nima.favdish.R
 import com.company.nima.favdish.databinding.ActivityAddUpdateDishBinding
 import com.company.nima.favdish.databinding.DialogCustomImageSelectionBinding
+import com.karumi.dexter.PermissionToken
+
+import com.karumi.dexter.listener.PermissionDeniedResponse
+
+import com.karumi.dexter.listener.PermissionGrantedResponse
+
+import com.karumi.dexter.listener.single.PermissionListener
+
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.DexterBuilder
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
+
 
 class AddUpdateDishActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var mBinding : ActivityAddUpdateDishBinding
 
+    companion object{
+        private const val CAMERA = 1
+        private const val GALLERY = 1
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mBinding = ActivityAddUpdateDishBinding.inflate(layoutInflater)
         setContentView(mBinding.root)
+
+        setSupportActionBar(mBinding.toolbarAddDishActivity)
+        val actionBar = supportActionBar
+        actionBar?.setDisplayHomeAsUpEnabled(true)
+        mBinding.toolbarAddDishActivity.setNavigationOnClickListener { onBackPressed() }
 
         mBinding.ivAddDishImage.setOnClickListener(this)
     }
@@ -32,11 +66,97 @@ class AddUpdateDishActivity : AppCompatActivity(), View.OnClickListener {
         val binding: DialogCustomImageSelectionBinding = DialogCustomImageSelectionBinding.inflate(layoutInflater)
         dialog.setContentView(binding.root)
         binding.tvCamera.setOnClickListener{
-            Toast.makeText(applicationContext, "camera selected", Toast.LENGTH_SHORT).show()
+            Dexter.withContext(this)
+                .withPermissions(Manifest.permission.CAMERA, Manifest.permission.READ_EXTERNAL_STORAGE)
+                .withListener(object : MultiplePermissionsListener {
+                    override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+                        report?.let {
+                            if (report.areAllPermissionsGranted()){
+                                val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                                startActivityForResult(intent, CAMERA)
+                            }
+                        }
+
+                    }
+
+                    override fun onPermissionRationaleShouldBeShown(
+                        permissions: MutableList<PermissionRequest>?,
+                        token: PermissionToken?
+                    ) {
+                        showRationalPermissionForShowPermission()
+                    }
+
+                }).onSameThread().check()
+            dialog.dismiss()
         }
         binding.tvGallery.setOnClickListener{
-            Toast.makeText(applicationContext, "gallery selected", Toast.LENGTH_SHORT).show()
+            Dexter.withContext(this)
+                .withPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                .withListener(object : PermissionListener {
+                    override fun onPermissionGranted(p0: PermissionGrantedResponse?) {
+                        val galleryIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                        startActivityForResult(galleryIntent, GALLERY)
+                    }
+
+                    override fun onPermissionDenied(p0: PermissionDeniedResponse?) {
+                        Toast.makeText(applicationContext, "you have denied storage permission", Toast.LENGTH_SHORT).show()
+                    }
+
+                    override fun onPermissionRationaleShouldBeShown(
+                        permission: PermissionRequest?,
+                        token: PermissionToken?
+                    ) {
+                        showRationalPermissionForShowPermission()
+                    }
+
+                }).onSameThread().check()
+            dialog.dismiss()
         }
         dialog.show()
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == RESULT_OK) {
+            if (requestCode == CAMERA) {
+                data?.extras?.let {
+                    val thumbnail: Bitmap = data.extras!!.get("data") as Bitmap
+                    mBinding.ivDishImage.setImageBitmap(thumbnail)
+
+                    mBinding.ivAddDishImage.setImageDrawable(
+                        ContextCompat.getDrawable(
+                            this,
+                            R.drawable.ic_vector_edit
+                        )
+                    )
+                }
+            }
+            if (requestCode == GALLERY){
+                data?.data.let {
+                    val selectedImageUri = data!!.data
+                    mBinding.ivDishImage.setImageURI(selectedImageUri)
+
+                    mBinding.ivAddDishImage.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.ic_vector_edit))
+                }
+            }
+        }
+    }
+
+    private fun showRationalPermissionForShowPermission() {
+        AlertDialog.Builder(this).setMessage("It looks like you don`t grant permission, go to setting for grant it")
+            .setPositiveButton("Go To Setting", DialogInterface.OnClickListener { _, _ ->
+                try {
+                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                    val uri = Uri.fromParts("package", packageName, null)
+                    intent.data = uri
+                    startActivity(intent)
+                }catch (e: ActivityNotFoundException){
+                    e.printStackTrace()
+                }
+            }).setNegativeButton("cancel", DialogInterface.OnClickListener { dialog, _ ->
+                dialog.dismiss()
+            }).show()
+    }
+
+
 }
