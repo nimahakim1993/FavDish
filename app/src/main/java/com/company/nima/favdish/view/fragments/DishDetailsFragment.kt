3912485a7fe2187078b16b87bc580b3a5a17f692
Nima <nimahakim.ad@gmail.com>
2022-03-19 14:33:49 +0330

    add palette for dish details

--- app/src/main/java/com/company/nima/favdish/view/fragments/DishDetailsFragment.kt
+++ app/src/main/java/com/company/nima/favdish/view/fragments/DishDetailsFragment.kt
@@ -1,12 +1,20 @@
 package com.company.nima.favdish.view.fragments
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.graphics.drawable.toBitmap
 import androidx.navigation.fragment.navArgs
+import androidx.palette.graphics.Palette
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import com.company.nima.favdish.R
 import com.company.nima.favdish.databinding.FragmentDishDetailsBinding
 import java.io.IOException
@@ -33,7 +41,26 @@ class DishDetailsFragment : Fragment() {
         val args : DishDetailsFragmentArgs by navArgs()
         args.let {
             try {
-                Glide.with(requireActivity()).load(it.dishDetails.image).into(mBinding!!.ivDishImage)
+                Glide.with(requireActivity()).load(it.dishDetails.image).
+                    listener(object :RequestListener<Drawable>{
+                        override fun onLoadFailed(e: GlideException?, model: Any?, target: Target<Drawable>?, isFirstResource: Boolean): Boolean {
+                            Log.e("log_error", "error loading image", e)
+                            return false
+                        }
+
+                        override fun onResourceReady(resource: Drawable?, model: Any?, target: Target<Drawable>?, dataSource: DataSource?, isFirstResource: Boolean): Boolean {
+                            resource.let {
+                                Palette.from(resource!!.toBitmap()).generate() {
+                                    palette ->
+                                    val intColor = palette?.vibrantSwatch?.rgb ?: 0
+                                    mBinding!!.rlDishDetailMain.setBackgroundColor(intColor)
+                                }
+                            }
+                            return false
+                        }
+
+                    }).
+                into(mBinding!!.ivDishImage)
             }
             catch (e: IOException){
                 e.printStackTrace()
